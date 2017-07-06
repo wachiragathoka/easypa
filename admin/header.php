ed159d4a90f6e9eb78be23a827c5505e42a355fd
Mathaayo Gathoka <wachiragathoka@gmail.com>
2017-07-06 20:49:48 +0300

    admin

---
+++ admin/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<!--<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+
+ <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> 
+
+
+<script type="text/javascript" src="./assets/js/pa.js"></script>-->
+<!--  <script type="text/javascript" src="application/assets/js/jquery.js"></script>-->
+<script type="text/javascript">
+</script>
+<title><?php echo constant("TITLE")?> | Quotes</title>
+</head>
+<body onload="step1()">
---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+<div class="row">
+		<div class="col-lg-3 data-cell side-header">Name</div>
+		<div class="col-lg-3 data-cell"><?php echo $customerData['0']?></div>
+		<div class="col-lg-3 data-cell side-header">Date of Birth</div>
+		<div class="col-lg-3 data-cell"><?php echo $customerData['1']?></div>
+	</div>
+
+
+<?php include 'footer.php';?>
